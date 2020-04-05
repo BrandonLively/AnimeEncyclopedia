9a901403761b4e09d7f041e9822577aec761a8a3
BrandonLively <akutenshi@live.com>
2020-04-05 15:49:33 -0400

    Query Testing with list and pagination setup

--- anime_encyclopedia/lib/core/data/query.dart
+++ anime_encyclopedia/lib/core/data/query.dart
@@ -1,12 +1,40 @@
 const String GET_TEST_ANIME = r'''
-query{
-  Media (id: 15125, type: ANIME) {
-    id
-    title {
-      romaji
-      english
-      native
+query
+{
+    Page (page: 1, perPage: 2) {
+        pageInfo {
+            total
+            currentPage
+            lastPage
+            hasNextPage
+            perPage
+        }
+        media{
+          title{
+            romaji
+            english
+          }
+          coverImage{
+            extraLarge
+          }
+          averageScore
+          startDate {
+            year
+            month
+            day
+          }
+          endDate {
+            year
+            month
+            day
+          }
+          episodes
+          description
+          bannerImage
+          
+      genres
+        }
     }
-  }
 }
+
 ''';
